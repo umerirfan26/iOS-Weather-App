35ae094ffe7c41d51c030cf105f04ebcbfb2ff2f
Muhammad Umer <https://github.com/umerirfan26>
2021-09-28 16:00:08 +0500

    complete ios weather app

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Muhammad Umer on 22/09/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Muhammad Umer on 24/09/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
